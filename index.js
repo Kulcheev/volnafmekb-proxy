9dc3829e57ab073ba63a32f4dbca3af33ebbbcdb
Kulcheev <162830825+Kulcheev@users.noreply.github.com>
2025-04-16 10:06:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const request = require('request');
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  const streamUrl = 'http://radio.volnafm.ru:8000/ekb';
+  req.pipe(request(streamUrl)).pipe(res);
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy listening on port ${PORT}`);
+});
